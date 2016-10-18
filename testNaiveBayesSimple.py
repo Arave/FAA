645dcafe78fc56c3bfb2cd0b01c5b47d6f0c6b5c
aSoler1 <albert.soler@estudiante.uam.es>
2016-10-18 22:25:11 +0200

    test naive bayes simple

---
+++ testNaiveBayesSimple.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 26 19:51:02 2016
+
+@author: Albert Soler, Alfonso Sebares
+"""
+
+from Datos import Datos
+from EstrategiaParticionado import ValidacionSimple
+from Clasificador import ClasificadorNaiveBayes
+
+print "Prueba 2- Fichero de datos d2.data"
+dataset=Datos('./ConjuntosDatosPruebas/d2.data',True)
+print "Estrategia: validacion simple, numParticiones: 1 porcentaje 80"
+estrategia=ValidacionSimple(1,80)
+#print estrategia.nombreEstrategia
+print "Clasificador: clasificador Naive Bayes"
+clasificador = ClasificadorNaiveBayes()
+print "Errores: "
+correccionL = True #aplicar correcion de Laplace a la tabla de train o no
+errores=clasificador.validacion(estrategia,dataset,clasificador,correccionL)
+
+
